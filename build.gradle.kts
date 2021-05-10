edb2c3c7ddc5a07c2382e606f2609d474937feea
patrickgiry <patrickgiry@gmail.com>
2021-05-10 22:17:29 +0200

    nit project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    groovy
     java
 }
 
@@ -21,7 +20,6 @@ java {
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.7.1"))
-    testImplementation("org.spockframework:spock-core:2.0-M4-groovy-3.0")
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.19.0")
 }
